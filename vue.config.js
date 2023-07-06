fbc5f0be5934cd98f21d87d58daf5e966c64c8f3
dengxiaojun <dxj1718874198@gmail.com>
2023-07-06 21:35:54 +0800

    [update]Visualized analysis

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,6 @@
 const webpack = require('webpack');
 const {defineConfig} = require('@vue/cli-service');
+const CompressionPlugin = require('compression-webpack-plugin');
 module.exports = defineConfig({
     publicPath: process.env.NODE_ENV === "production" ? "./" : "/",
     transpileDependencies: true,
@@ -9,6 +10,11 @@ module.exports = defineConfig({
             new webpack.ProvidePlugin({
                 process: 'process/browser',
             }),
+            new CompressionPlugin({
+                test: /\.js$|\.html$|\.css/,
+                threshold: 10240,
+                deleteOriginalAssets: false
+            })
         ],
         resolve: {
             fallback: {
